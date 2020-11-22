5cde68fd3bf5c2cabb3ed49da1dc6a7e7171f0ed
Ablay <AYedilbayev@one.kz>
2020-11-22 20:27:06 +0600

    feat: create retrofit instance and launch object DTO

---
+++ app/src/main/java/com/example/spaceapp/ApiClient.kt
@@ -0,0 +1,29 @@
+package com.example.spaceapp
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+private const val BASE_URL = "https://ll.thespacedevs.com/2.0.0/"
+
+class ApiClient {
+
+    fun getClient(): Retrofit? {
+
+        val interceptor = HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val client: OkHttpClient = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
+}
